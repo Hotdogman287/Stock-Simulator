c64602746192e2a5e501faa5bcd59e1d0d970c5d
Hotdogman287 <mbruhl931@gmail.com>
2026-03-20 21:13:31 -0400

    Update Stock Simulator.py

--- Stock Simulator.py
+++ Stock Simulator.py
@@ -10,7 +10,7 @@ window_height = 450
 #colors
 red = (255, 0, 0)
 green = (0, 255, 0)
-
+grey = (30, 30, 30)
 
 
 
@@ -21,20 +21,43 @@ pygame.display.set_caption("Stock Market Sim")
 
 price_change = .2
 drawn = 0
-price_list = {100, 110, 130, 90}
+price_list = [10, 5, 13, 9, 6, 10, 15, 21]
 prices = len(price_list)
-drawn_prices = 0
+price_index = 0
+
+#initial bar
+x = 100
+y = window_height/2
+
+
+y_change = 0
+
+
+pygame.draw.line(screen, grey, [x, y], [x+400, y])
 
 
 #loop
 running = True
 while running:
 
-        while drawn_prices < prices:
+        #drawing prices
+        while price_index < prices:
             #draw to the screen
-            pygame.draw.rect(screen, red, [100, 100, 400, 100], 0) #this is drawing the first rectangle
-            drawn_prices += 1
-        
+            if y_change >= 0:
+                bar_color = red
+            else:
+                  bar_color = green
+
+
+
+            pygame.draw.rect(screen, bar_color, [x, y, 20, y_change], 0) #this is drawing the first rectangle
+            y_change = price_list[price_index]
+            #y_change = price_list[price_index] - y_change
+            #y = y - y_change
+            x += 20
+            print("Set price", price_index, "at", x, "change of", y)
+            
+            price_index += 1
 
         #end game
         for event in pygame.event.get():
